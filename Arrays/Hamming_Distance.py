1657848d6d1f6076c6bc1c12141b3c146cb0426f
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-05-14 22:57:08 +0100

    hamming distance

---
+++ Arrays/Hamming_Distance.py
@@ -0,0 +1,48 @@
+"""
+Problem Statement                                            
+  Have the function HammingDistance(strArr) take the array of  
+  strings stored in strArr, which will only contain two strings
+  of equal length and return the Hamming distance between them.
+  The Hamming distance is the number of positions where the    
+  corresponding characters are different.                      
+  For example: if strArr is ["coder", "codec"] then your       
+  program should return 1. The string will always be of equal  
+  length and will only contain lowercase characters from the   
+  alphabet and numbers.                                        
+                                                               
+  Examples                                                     
+  Input 1: ["10011", "10100"]                                  
+  Output 1: 3                                                  
+                                                               
+  Input 2: ["helloworld", "worldhello"]                        
+  Output 2: 8                     
+"""
+
+"""
+The problem requires us to find the Hamming distance between two strings 
+of equal length. The Hamming distance is the number of positions where 
+the corresponding characters are different. 
+
+One approach to solve this problem is to iterate through the two strings 
+and compare the characters at each position. If the characters at the 
+current position are different, we increment a counter. Finally, 
+we return the counter as the Hamming distance.
+
+Here is the algorithm:
+
+1. Convert the two strings in the input array into character arrays.
+2. Initialize a counter variable to 0.
+3. Iterate through the character arrays:
+   a. If the character at the current position in the first array is different from the character at the corresponding position in the second array, increment the counter.
+4. Return the counter as the Hamming distance.
+"""
+
+
+def HammingDistance(strArr):
+    return sum(ch1 != ch2 for ch1, ch2 in zip(strArr[0], strArr[1]))
+
+
+print(HammingDistance(["10011", "10100"]))  # 3
+print(HammingDistance(["helloworld", "worldhello"]))  # 8
+print(HammingDistance(["abcdefg", "ABCDEFG"]))  # 7
+print(HammingDistance(["abcd1234", "abdc2143"]))  # 6
